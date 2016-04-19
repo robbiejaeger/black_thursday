de452e27835820db586d8e590aea6e6d6d6536c8
Peter Springer <peterspringer829@gmail.com>
2016-04-19 12:21:10 -0600

    iteration 0

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -5,16 +5,18 @@ class SalesEngine
 
   attr_reader :items, :merchants
 
-  def initialize(csv_file_path)
-    @items = ItemRepository.new(csv_file_path)
-    @merchants = MerchantRepository.new(csv_file_path)
-  end
+  # def initialize(csv_file_path)
+  #   @items = ItemRepository.new(csv_file_path[:items])
+  #   @merchants = MerchantRepository.new(csv_file_path[:merchants])
+  # end
 
   def self.from_csv(csv_file_path)
+    @items = ItemRepository.new(csv_file_path[:items])
+    @merchants = MerchantRepository.new(csv_file_path[:merchants])
     # SalesEngine.new(hash)
-   {:items => "./data/items.csv",
-   :merchants => "./data/merchants.csv"
-  }
+  #  {:items => "./data/items.csv",
+  #  :merchants => "./data/merchants.csv"
+  # }
     #SalesEngine.new ??
     # returns Hash
   end
@@ -25,11 +27,14 @@ class SalesEngine
 
 
 end
+#
+# se = SalesEngine.from_csv(({:items => "./data/items.csv",
+#    :merchants => "./data/merchants.csv"
+#   }))
+#
+# puts se.from_csv({:items => "./data/items.csv",
+#    :merchants => "./data/merchants.csv"
+#   })
 
-se = SalesEngine.from_csv(({:items => "./data/items.csv",
-   :merchants => "./data/merchants.csv"
-  }))
-
-puts se.from_csv({:items => "./data/items.csv",
-   :merchants => "./data/merchants.csv"
-  })
+se = SalesEngine.from_csv("./data/items.csv")
+puts se
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,12 +1,13 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
 
   def test_from_csv_creates_SE_object
-    se = SalesEngine
-
+    se = SalesEngine.new
+    assert se.items("./data/items.csv")
   end
 
 
