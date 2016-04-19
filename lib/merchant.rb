bf7438a0aa9a2eb203ee77501068499a3f24fad9
Luigi Aversano <luigiaversano@gmail.com>
2016-04-19 14:16:17 -0600

    finished iteration 0

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -10,15 +10,4 @@ attr_reader :id, :name
     @id = merchant_hash[:id].to_i
     @name = merchant_hash[:name]
   end
-
-
-
-  # The merchant is one of the critical concepts in our
-  # data hierarchy.
-  # id - returns the integer id of the merchant
-  # name - returns the name of the merchant
-  # We create an instance like this:
-
-  # m = Merchant.new({:id => 5, :name => "Turing School"})
-
 end
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,11 +1,13 @@
 require "minitest/autorun"
 require "minitest/pride"
-
+require "pry"
+require_relative "../lib/merchant"
 
 class MerchantsTest < Minitest::Test
 
+  def test_creates_mechant
+    merch = Merchant.new(:id => 555, :name => "Turing School")
+    assert merch.kind_of?(Merchant)
+  end
   
-
-
-
 end
