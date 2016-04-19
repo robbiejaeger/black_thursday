96ec9a9119eb400e411d4a0ea9d28a7ae1913ff0
Peter Springer <peterspringer829@gmail.com>
2016-04-19 10:35:40 -0600

    start of sales engine class

---
+++ lib/sales_engine.rb
@@ -0,0 +1,35 @@
+# require_relative 'merchant_repository'
+# require_relative 'item_repository'
+
+class SalesEngine
+
+  attr_reader :items, :merchants
+
+  def initialize(csv_file_path)
+    @items = ItemRepository.new(csv_file_path)
+    @merchants = MerchantRepository.new(csv_file_path)
+  end
+
+  def self.from_csv(csv_file_path)
+    # SalesEngine.new(hash)
+   {:items => "./data/items.csv",
+   :merchants => "./data/merchants.csv"
+  }
+    #SalesEngine.new ??
+    # returns Hash
+  end
+  #figure out how the salesengine object
+  #is created from the from_csv method
+
+
+
+
+end
+
+se = SalesEngine.from_csv(({:items => "./data/items.csv",
+   :merchants => "./data/merchants.csv"
+  }))
+
+puts se.from_csv({:items => "./data/items.csv",
+   :merchants => "./data/merchants.csv"
+  })
---
+++ test/sales_engine_test.rb
@@ -0,0 +1,19 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class SalesEngineTest < Minitest::Test
+
+  def test_from_csv_creates_SE_object
+    se = SalesEngine
+
+  end
+
+
+
+
+
+
+
+
+end
