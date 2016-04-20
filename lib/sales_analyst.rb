b367e21c290203d555cfb357c452de8d428ec5d4
Robbie Jaeger <jaeger.rob@gmail.com>
2016-04-20 08:07:23 -0600

    Add sales analyst avergage, stdev, and high item count methods and tests

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,20 +1,30 @@
-require_relative "../lib/sales_engine"
 class SalesAnalyst
 
-    def average_items_per_merchant
-      
+  attr_reader :sales_engine
+
+  def initialize(sales_engine)
+    @sales_engine = sales_engine
+  end
+
+  def average_items_per_merchant
+    (@sales_engine.items.all.count.to_f/@sales_engine.merchants.all.count.to_f).round(2)
+  end
+
+  def average_items_per_merchant_standard_deviation
+    avg = average_items_per_merchant
+    Math.sqrt(@sales_engine.merchants.all.map do |merchant|
+      (merchant.items.count - avg) ** 2
+    end.reduce(:+)/@sales_engine.merchants.all.count.to_f).round(2)
+  end
+
+  def merchants_with_high_item_count
+    stdev = average_items_per_merchant_standard_deviation
+    @sales_engine.merchants.all.find_all do |merchant|
+      merchant.items.count > (1 + stdev)
     end
+  end
 
-
-
-
-
-
-
-
-
-
+  def average_item_price_for_merchant(merchant_id)
+  end
 
 end
-
-sa = SalesAnalyst.new(se)
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -6,16 +6,35 @@ require_relative "../lib/sales_engine"
 
 class SalesAnalystTest < Minitest::Test
 
+  def setup
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
 
+    @sa = SalesAnalyst.new(se)
+  end
 
+  def test_it_can_exist
+    assert @sa
+  end
 
+  def test_can_average_items_per_merchant
+    avg = @sa.average_items_per_merchant
 
+    assert_equal 2.88, avg
+  end
 
+  def test_can_get_standard_deviation
+    stdev = @sa.average_items_per_merchant_standard_deviation
 
+    assert_equal 3.26, stdev
+  end
 
+  def test_can_get_merchants_with_high_item_count
+    merchs = @sa.merchants_with_high_item_count
 
-
-
-
+    assert_equal 85, merchs.count
+  end
 
 end
