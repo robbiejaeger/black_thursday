422e4fa21ba17832536237cb60f87039cc06ef90
Luigi Aversano <luigiaversano@gmail.com>
2016-04-21 13:27:56 -0600

    more methods and tests in iteration 2

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -17,10 +17,18 @@ class InvoiceRepositoryTest < Minitest::Test
   end
 
   def test_find_by_id
-
+    refute @ir.find_by_id(46666000)
     assert_equal 793, @ir.find_by_id(4000).customer_id
   end
 
+  def test_find_all_by_customer_id
+    assert_equal [], @ir.find_all_by_customer_id(5555)
+    assert_equal 8, @ir.find_all_by_customer_id(1).count
+  end
 
+  def test_find_all_by_merchant_id
+    assert_equal [], @ir.find_all_by_merchant_id(2139875)
+    assert_equal 16 ,@ir.find_all_by_merchant_id(12335938).count 
+  end
 
 end
