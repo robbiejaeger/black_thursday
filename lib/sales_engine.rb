a9592830518f79c4550bc1fc52c77119ee1cef5f
Peter Springer <peterspringer829@gmail.com>
2016-04-23 15:39:15 -0600

    transactions two

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,13 +3,12 @@ require_relative 'item_repository'
 require_relative 'invoice_repository'
 
 class SalesEngine
-  attr_reader :items, :merchants, :invoices, :transactions
+  attr_reader :items, :merchants, :invoices
 
   def initialize(hash)
     @items = ItemRepository.new(hash[:items], self)
     @invoices = InvoiceRepository.new(hash[:invoices], self)
     @merchants = MerchantRepository.new(hash[:merchants], self)
-    @transactions = TransactionRepository.new(hash[:merchants], self)
   end
 
   def self.from_csv(hash)
