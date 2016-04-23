c668f4003ab7e0cde8c1f1cbdd05cb00dde741ae
Luigi Aversano <luigiaversano@gmail.com>
2016-04-23 15:45:52 -0600

    test for customer class

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -5,8 +5,16 @@ require_relative "../lib/customer_repository"
 
 class CustomerTest < Minitest::Test
 
+  def setup
+    csv_filepath = "./data/invoices.csv"
+    @cr = CustomerRepository.new(csv_filepath, nil)
+  end
 
+  def test_customer_id_is_an_integer
+    customer1 = @cr.all[0]
 
+    assert customer1.id.kind_of?(Fixnum)
+  end
 
 
 end
