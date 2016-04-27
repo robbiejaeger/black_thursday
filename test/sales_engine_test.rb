685284732a7220303ecad4b9ce347d240f7122cb
Robbie Jaeger <jaeger.rob@gmail.com>
2016-04-27 09:35:35 -0600

    Add sales engine tests

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -8,7 +8,23 @@ class SalesEngineTest < Minitest::Test
       :merchants => "./data/merchants.csv"
       })
 
-    assert se
+    assert se.kind_of?(SalesEngine)
+  end
+
+  def test_engine_has_an_item_repo
+    se = SalesEngine.from_csv({:items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+      })
+
+    assert se.items.kind_of?(ItemRepository)
+  end
+
+  def test_engine_has_an_merchant_repo
+    se = SalesEngine.from_csv({:items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+      })
+
+    assert se.merchants.kind_of?(MerchantRepository)
   end
 
 end
